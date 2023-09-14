f577b315f538ab323437010fdadabed1135e6201
Nefta007 <nperc001@ucr.edu>
2023-09-14 01:45:58 -0700

    added prompt as well as message that indicates a logo was generated

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const inquirer = require('inquirer');
+
+const init = () =>{
+    inquirer.prompt([{
+        type: 'input',
+        name: 'svg',
+        message: 'Enter a three letter text.',
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: 'Please enter a text color.',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'please choose a desired shape.',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapes-color',
+        message: 'Please enter a color for the shape.',
+    }]).then((answers)=>{
+    console.log('Generated logo.svg');
+});
+}
+init();
